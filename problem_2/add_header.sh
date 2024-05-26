9149d9dd22620ec7ba7b25d97bf28b102f2ba892
jt1975sc <148169798+jt1975sc@users.noreply.github.com>
2024-05-26 16:00:57 -0700

    Update add_header.sh

--- problem_2/add_header.sh
+++ problem_2/add_header.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
+files_i_care_about=( *.c )
 
-files_i_care_about=( $(ls *.c))
-
-for t in ${!files_i_care_about[@]}; do
-    echo '**Propety of Blaa.co 1882**' |  cat - ${files_i_care_about[$t]} > temp && mv temp ${files_i_care_about[$t]}
+for file in "${files_i_care_about[@]}"; do
+    echo '**Property of Blaa.co 1882**' | cat - "$file" > temp && mv temp "$file"
 done
+
